06face0b7349e2909468c88d4094745d2c702991
JhoLee <jho3689@a.ut.ac.kr>
2017-09-11 17:23:02 +0900

    Add files via upload

---
+++ A/src/A.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class A {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// Number of test cases
+		int T = sc.nextInt();
+
+		for (int i = 0; i < T; i++) {
+
+			// Data Input
+			int H = sc.nextInt(); // Number of floors
+			int W = sc.nextInt(); // Number of rooms in single floor
+			int N = sc.nextInt(); // The order of guest
+
+			// Computing..
+			int height = N % H;
+			int weight = 1 + (N / H);
+
+			// Printing..
+			System.out.print(height);
+			System.out.printf("%02d", weight);
+			System.out.println();
+
+		}
+		
+		
+
+	}
+
+}
